481118b00b3e33e4e570730b93def4752f3a7f16
AsaMitaka <cepelits@gmail.com>
2024-01-08 19:00:53 +0200

    * hello, world

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,7 @@
+import { zValidator } from '@hono/zod-validator';
 import { Hono } from 'hono';
 import { handle } from 'hono/vercel';
-import { html } from 'hono/html';
+import { z } from 'zod';
 
 export const config = {
   runtime: 'edge',
@@ -8,25 +9,39 @@ export const config = {
 
 const app = new Hono().basePath('/api');
 
-app.get('/hello', (c) => {
-  return c.json({ message: 'Hello Hono!' });
+app.get('/world', (c) => {
+  return c.json({ message: 'Hello World!' });
 });
 
-app.get('/test', (c) => {
-  const blockedCountryCode = ['UA'];
-  const incomingCountryCode = c.req.header('x-vercel-ip-country');
-
-  if (blockedCountryCode.includes(incomingCountryCode?.toUpperCase())) {
-    return c.html(
-      html`<!DOCTYPE html>
-        <h1>Hello! U from ${incomingCountryCode}! U have blocked!</h1>`,
-    );
-  }
-
-  return c.html(
-    html`<!DOCTYPE html>
-      <h1>Hello! U from ${incomingCountryCode}!</h1>`,
-  );
-});
+app.get(
+  '/hello',
+  zValidator(
+    'query',
+    z.object({
+      name: z.string(),
+    }),
+  ),
+  (c) => {
+    const { name } = c.req.valid('query');
+    return c.json({ message: `Hello ${name}` });
+  },
+);
 
 export default handle(app);
+
+// app.get('/test', (c) => {
+//   const blockedCountryCode = ['US', 'RU', 'BG', 'CN'];
+//   const incomingCountryCode = c.req.header('x-vercel-ip-country') || 'UA';
+
+//   if (blockedCountryCode.includes(incomingCountryCode.toUpperCase())) {
+//     return c.html(
+//       html`<!DOCTYPE html>
+//         <h1>Hello! U from ${incomingCountryCode}! U have blocked!</h1>`,
+//     );
+//   }
+
+//   return c.html(
+//     html`<!DOCTYPE html>
+//       <h1>Hello! U from ${incomingCountryCode}!</h1>`,
+//   );
+// });
