b72429f272a6c193bae5137bd1dfd98ddd290d64
AsaMitaka <cepelits@gmail.com>
2024-01-08 19:26:20 +0200

    fake db

---
+++ assets/db.ts
@@ -0,0 +1,19 @@
+import { DataProps } from '../interface/data.interface';
+
+export const data: DataProps[] = [
+  {
+    id: 1,
+    name: 'I',
+    number: '12345678',
+  },
+  {
+    id: 2,
+    name: 'D',
+    number: '12345678',
+  },
+  {
+    id: 3,
+    name: 'G',
+    number: '12345678',
+  },
+];
