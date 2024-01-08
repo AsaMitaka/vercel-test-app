e73188f1c803b1eba0fdb2fcbdb6eba3412b352b
AsaMitaka <cepelits@gmail.com>
2024-01-08 20:00:50 +0200

    + hello route

---
+++ routes/hello.route.ts
@@ -0,0 +1,21 @@
+import { Hono } from 'hono';
+import { z } from 'zod';
+import { zValidator } from '@hono/zod-validator';
+
+const helloRoute = new Hono();
+
+helloRoute.get(
+  '/hello',
+  zValidator(
+    'query',
+    z.object({
+      name: z.string(),
+    }),
+  ),
+  (c) => {
+    const { name } = c.req.valid('query');
+    return c.json({ message: `Hello ${name}` });
+  },
+);
+
+export default helloRoute;
